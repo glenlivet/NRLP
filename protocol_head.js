acf5c8642e9c35510e3be3bb42eb419a5c59752f
glenlivet <kikyou1986@hotmail.com>
2014-06-04 18:00:48 +0800

    ff

--- protocol_head.js
+++ protocol_head.js
@@ -5,6 +5,19 @@ var bops	= require('bops');
 var PROTOCOL_NAME = 'NRLP';
 var PROTOCOL_VERSION = 1;
 
+//广播请求
+var PROTOCOL_BROADCAST_REQUEST = 1;
+//提供方案
+var PROTOCOL_PROPOSAL_PROVISION = 2;
+//请求资源
+var PROTOCOL_RESOURCE_REQUEST = 3;
+//请求答复
+var PROTOCOL_REQUEST_PERMISSION = 4;
+//请求开始传输
+var PROTOCOL_REQUEST_START = 5;
+//传输开始
+var PROTOCOL_TRANSFER_START = 6;
+
 /**
  * 
  */
@@ -21,15 +34,20 @@ var getProtocolVersionBuffer = function(){
 	return buf;
 };
 
-var getProtocolHead = function(){
-	return bops.join([getProtocolNameBuffer(), getProtocolVersionBuffer()]);
+var getProtocolHead = function(type){
+	return bops.join([getProtocolNameBuffer(), getProtocolVersionBuffer(), getMessageTypeBuffer(type)]);
+};
+
+var getMessageTypeBuffer = function(type){
+	var buf = new Buffer(1);
+	buf.writeInt8(type, 0);
+	return buf;
 };
 
 
-
-// var buf = getProtocolHead();
-// console.log(buf.length);
-// for(var i = 0 ; i < buf.length; i++){
-	// console.log(buf[i]);
-// }
+var buf = getProtocolHead(PROTOCOL_REQUEST_PERMISSION);
+console.log(buf.length);
+for(var i = 0 ; i < buf.length; i++){
+	console.log(buf[i]);
+}
 
